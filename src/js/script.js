4e04bd5db7003fc61c6341a51e6a7a57e279ed71
Patryk Zahorski <maarcane@gmail.com>
2019-09-20 08:19:42 +0200

    add second web

--- src/js/script.js
+++ src/js/script.js
@@ -6,69 +6,33 @@ window.onload = function() {
 	
 	let E_main_nav = document.querySelector("#main_nav")
 
-	window.onscroll = () => {
-		if(window.scrollY>100&!E_main_nav.classList.contains("nav_top")){
-			E_main_nav.style.transition = 'none';
-			E_main_nav.style.top = "-100px"
-			E_main_nav.classList.add("nav_top")
-			E_main_nav.offsetHeight // Trigger a reflow, flushing the CSS changes
-			
-			E_main_nav.style.transition = '';
-			E_main_nav.style.top = "0"
-		
+	window.onscroll = nav_top_update
+	window.onresize = nav_top_update
+	
+	function nav_top_update() {
+		let is_top = E_main_nav.classList.contains("nav_top")
+		if( window.innerWidth<=800 || window.scrollY>=100){
+			if( !is_top ){
+				E_main_nav.style.transition = 'none';
+				E_main_nav.style.top = "-100px"
+				E_main_nav.classList.add("nav_top")
+				E_main_nav.offsetHeight // Trigger a reflow, flushing the CSS changes
+				
+				E_main_nav.style.transition = '';
+				E_main_nav.style.top = "0"
+			}
+		}else if( window.innerWidth>800 || window.scrollY<100){
+			if( is_top ){
+				E_main_nav.classList.remove("nav_top")
+				E_main_nav.style.transition = 'none';
+				E_main_nav.style.top = "-100px"
+				E_main_nav.offsetHeight // Trigger a reflow, flushing the CSS changes
+				E_main_nav.style.transition = '';
+				
+				E_main_nav.style.top = "0"
+			}
 		}
-		if(window.scrollY<100&E_main_nav.classList.contains("nav_top")){
-			E_main_nav.classList.remove("nav_top")
-			E_main_nav.style.transition = 'none';
-			E_main_nav.style.top = "-100px"
-			E_main_nav.offsetHeight // Trigger a reflow, flushing the CSS changes
-			E_main_nav.style.transition = '';
-			
-			E_main_nav.style.top = "0"
-		}
-	}
-
-	/*
-	window.ontouchstart = (event) => {
-    event.preventDefault()
-    event.stopPropagation()
-
-		//console.log(event)
 	}
 	
-	let lastTarger = window
-	window.onmouseover = (event) => {
-		event.preventDefault()
-    event.stopPropagation()
-		//lastTarger.blur()
-		//lastTarger = event.target
-		//event.target.focus()
-    //event.preventDefault()
-		console.log(event)
-	}
-
-	let E_main = document.querySelector("#main")
-	let E_home = document.querySelector("#home")
-	let img = new Image
-	img.src = getComputedStyle(E_home)['background-image'].replace(/url\(.|.\)$/ig, "");
-	console.log(img,img.src)
-	// 2000x1807   2000x86
-	function onresize() {
-		h1 = E_main.clientHeight
-		w1 = E_main.clientWidth
-		var imgW = img.width
-		var imgH = img.height
-		if(imgW > imgH){
-			newW = E_home.clientWidth //100;
-			newH = imgH / imgW * newW;
-			}else{
-			newH = E_home.clientHeight //100
-			newW = imgW / imgH * newH
-		}
-		console.log(newW+':'+newH);
-		//h2 = E_home.clientHeight
-		//w2 = E_home.clientWidth
-	}
-	window.onresize = onresize()
-	*/
+	nav_top_update()
 }
